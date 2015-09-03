3cfe55d08759e4ea5573798ab6cb911473c60fcd
Calvin <calvinliu24@gmail.com>
2015-09-03 02:24:16 -0700

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,1 @@
-module ApplicationHelper
 
-	#Return the full title on a per-page basis.
-	def full_title(page_title = '')
-		base_title = "Twitter Clone"
-		if page_title.empty?
-			base_title
-		else
-			page_title + " | " + base_title
-		end
-	end
-end
